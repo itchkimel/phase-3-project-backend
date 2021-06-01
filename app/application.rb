8be037ed7d68724e5f1070cff1b3b4eb91f5fc7a
Yitzchok Kimelman <itchkimel@gmail.com>
2021-06-01 17:07:10 -0400

    added a req method

--- app/application.rb
+++ app/application.rb
@@ -9,17 +9,31 @@ class Application
     elsif req.path.match("/guitars") # regex as a string
       # get toys from DB
       #serve to client as json 
-      send_toys
+      send_guitars
+    elsif req.path.match("/customers") # regex as a string
+      # get toys from DB
+      #serve to client as json 
+      send_guitars
     else
       send_not_found
     end
   end
 
   private
-  def send_toys
+  
+  def send_guitars
+    customer_inst_arr = Customer.all
+    return [200, { "Content-Type" => "application/json" }, [ customer_inst_arr.to_json ]]
+  end
+
+  def send_customers
     guitar_inst_arr = Guitar.all
     return [200, { "Content-Type" => "application/json" }, [ guitar_inst_arr.to_json ]]
   end
+
+  # def customer
+  #   Customer.where(name: 'Ricky Barton')
+  # end
 
   def send_hello
     return [200, { "Content-Type" => "application/json" }, [{ :message => "hello world!" }.to_json]]
