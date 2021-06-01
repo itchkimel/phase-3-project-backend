e79f3a421dcf61fb6361fffc2cb0af4f053f07fa
Yitzchok Kimelman <itchkimel@gmail.com>
2021-06-01 01:05:21 -0400

    Rack is starting to take form

--- app/application.rb
+++ app/application.rb
@@ -2,14 +2,24 @@ class Application
   def call(env)
     req = Rack::Request.new(env)
 
-    if req.path.match(/hello/)
+    # binding.pry
+
+    if req.path.match(/hello/) #regex
       send_hello
+    elsif req.path.match("/guitars") # regex as a string
+      # get toys from DB
+      #serve to client as json 
+      send_toys
     else
       send_not_found
     end
   end
 
   private
+  def send_toys
+    guitar_inst_arr = Guitar.all
+    return [200, { "Content-Type" => "application/json" }, [ guitar_inst_arr.to_json ]]
+  end
 
   def send_hello
     return [200, { "Content-Type" => "application/json" }, [{ :message => "hello world!" }.to_json]]
